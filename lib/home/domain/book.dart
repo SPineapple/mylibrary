eb184f8254240c82a20402a8b9ba3c10ac89d60c
SPineapple <drop-ikha@hotmail.com>
2023-12-17 23:18:24 +0300

    added missing category

--- lib/home/domain/book.dart
+++ lib/home/domain/book.dart
@@ -2,12 +2,14 @@ class Book {
   final String title;
   final String author;
   final String? description;
+  final String? category;
   final String coverUrl;
 
   Book({
     required this.title,
     required this.author,
     this.description,
+    this.category,
     required this.coverUrl,
   });
 }
